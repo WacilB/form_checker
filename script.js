2e5237f5a818de8730d722ab4a5de4e1b8dad23b
Wacil <wacil.b@icloud.com>
2022-02-10 11:53:28 +0100

    Pseudo checker

---
+++ script.js
@@ -0,0 +1,55 @@
+const inputs = document.querySelectorAll(
+  "input[type='text'], input[type='password'"
+);
+let pseudo, email, password, confirmPass;
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
+const pseudoChecker = (value) => {
+  if (value.length > 1 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "Le Pseudo doit contenir entre 2 et 20 caractères");
+    pseudo = null
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractère spéciaux"
+      );
+      pseudo = null
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = e.target.value
+  }
+};
+const emailChecker = (value) => {};
+const passwordChecker = (value) => {};
+const confirmChecker = (value) => {};
+
+inputs.forEach((inputs) => {
+  inputs.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
+});
